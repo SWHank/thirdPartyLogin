3cbb63a8bb85637743c1787af7f38772916cfb16
Lee <lee@LeedeMacBook-Pro.local>
2015-01-05 17:53:07 +0800

    Initial Commit

---
+++ thirdPartyLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thirdPartyLogin
+//
+//  Created by Lee on 2015/1/5.
+//  Copyright (c) 2015年 Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
